c39eb4313c2d5033f7c03cd19a8cb6fd6ec0e9a6
es6468s <enes.sen@alumni.fh-aachen.de>
2023-11-19 20:40:04 +0100

    Updated config

--- Kill.cs
+++ Kill.cs
@@ -1,23 +1,42 @@
-﻿
-using System.Drawing;
-using CounterStrikeSharp.API;
+﻿using System.Text.Json.Serialization;
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Core.Attributes.Registration;
 
 namespace Preach.CS2.Plugins.Kill;
-public class Kill : BasePlugin
+
+public class Kill : BasePlugin, IPluginConfig<KillConfig>
 {
     public override string ModuleName => "Kill";
-    public override string ModuleVersion => "0.1.0";
+    public override string ModuleVersion => "0.2.0";
     public override string ModuleAuthor => "Preach";
+
+    required public KillConfig Config {get; set; }
 
     public override void Load(bool hotReload)
     {
-        AddCommand("suicide", "kill yourself", (ply, info) => {
-            if(ply == null || !ply.IsValid || !ply.PlayerPawn.IsValid)
+        AddCommand("suicide", "Kill yourself", (ply, info) => {
+            if(ply == null || !ply.IsValid || !ply.PawnIsAlive || !ply.PlayerPawn.IsValid)
                 return;
+
+            if(!Config.ToggleCommand)
+            {
+                ply.PrintToChat("[Kill] Command is disabled");
+                return;
+            }
 
             ply.PlayerPawn.Value.CommitSuicide(true, false);
         });
+
     }
+
+    public void OnConfigParsed(KillConfig config)
+    {
+        Config = config;
+    }
+}
+
+public class KillConfig : BasePluginConfig
+{
+    [JsonPropertyName("Toggle Command")]
+    public bool ToggleCommand { get; set; } = true;
+
 }
